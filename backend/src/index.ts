9ae7ac07e64e6d0e1237224b0f516f0434352c4e
Tiago Abreu Laibacas <tiago.abreu.laibacas@gmail.com>
2025-08-30 02:16:30 +0100

    ✨ Add initial backend items

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server = Fastify({ logger: false });
+
+const PORT = Number(process.env.PORT);
+const HOST = process.env.HOST;
+const CORS_ORIGIN = process.env.CORS_ORIGIN?.split(",").map((o) => o.trim());
+
+await server.register(cors, { origin: CORS_ORIGIN });
+
+server.get("/", async () => {
+  return {
+    status: "ok",
+    message: "Server running with Fastify",
+    timestamp: Date.now(),
+  };
+});
+
+await server.listen({ port: PORT, host: HOST });
+
+console.log(`Server running at http://${HOST}:${PORT}`);
