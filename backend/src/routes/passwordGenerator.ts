8e527cee383a207f80faf688a697a3943ccfe817
Tiago Abreu Laibacas <tiago.abreu.laibacas@gmail.com>
2025-09-04 15:00:55 +0100

    🐛 Fix: improve error handling in password generator route

--- backend/src/routes/passwordGenerator.ts
+++ backend/src/routes/passwordGenerator.ts
@@ -1,25 +1,32 @@
 import { FastifyInstance } from "fastify";
+import { ZodError } from "zod";
 import { passwordSchema } from "../validator/passwordValidator.js";
 import { generateSecurePassword } from "../services/passwordService.js";
 
 export default async function passwordGeneratorRoutes(
   fastify: FastifyInstance
 ) {
-  fastify.get("/password-generator", async (request, reply) => {
+  fastify.get("/password-generator", async () => {
     return { message: "You wont get nothing with GET/ try POST/!" };
   });
+
   fastify.post("/password-generator", async (request, reply) => {
     try {
       const parsedBody = passwordSchema.parse(request.body);
-
       const password = generateSecurePassword(parsedBody);
-
       return { password };
     } catch (err) {
-      if (err instanceof Error) {
-        return reply.status(400).send({ error: err.message });
+      if (err instanceof ZodError) {
+        return reply
+          .status(400)
+          .send({ error: err.issues[0]?.message || "Validation error" });
       }
-      return reply.status(400).send({ error: "unknown error." });
+
+      if (err instanceof Error) {
+        return reply.status(500).send({ error: err.message });
+      }
+
+      return reply.status(500).send({ error: "unknown error." });
     }
   });
 }
